412bbe1f1e5007f5aa1b04c27a84dd8274fd98f3
Ryan Daley <daley.p.ryan@gmail.com>
2023-12-11 17:09:48 +0100

    PostDetailPages

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from datetime import date
 
-posts = [
+all_posts = [
     {
         "slug": "coral-house",
         "image": "houseRendering.png",
@@ -60,14 +60,19 @@ def get_date(post):
     return post['date']
 
 def starting_page(request):
-    sorted_posts = sorted(posts, key=get_date)
+    sorted_posts = sorted(all_posts, key=get_date)
     latest_posts = sorted_posts[-3:]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 def post_details(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post
+    })
