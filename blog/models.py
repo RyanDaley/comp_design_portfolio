9747814202f9c18f64d931e91a62c478c20b68c6
Ryan Daley <daley.p.ryan@gmail.com>
2024-01-12 12:10:06 +0100

    ViewsAndTemplates

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,21 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
 
 class Project(models.Model):
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
+    description = models.CharField(null=True, max_length=1000)
+    slug = models.SlugField(default="", null=False, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse("project-detail-page", args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
