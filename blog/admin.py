a00327908a529dcc26b3b7fb3a6a7b7fa89e4da3
Ryan Daley <daley.p.ryan@gmail.com>
2024-01-14 17:18:41 +0100

    NewModels

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Project, Author, Address
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("title",)}
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,36 @@ from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return f"{self.street} {self.postal_code} {self.city}"
+    
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
+
 class Project(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(null=True, max_length=1000)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True, editable=False, null=False, db_index=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="projects")
 
     def get_absolute_url(self):
         return reverse("project-detail-page", args=[self.slug])
@@ -18,4 +44,4 @@ class Project(models.Model):
 
 
     def __str__(self):
-        return f"{self.title} ({self.rating})"
+        return f"{self.title}"
